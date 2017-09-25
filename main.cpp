fee33ba3ed95d56d3d0b713d672e28568a47c212
jakublada <kuba.lada.1996@wp.pl>
2017-09-25 15:36:33 +0200

    cwiczenie2

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int rok_urodzenia;
+ cout << "Prosze podac rok urodzenia "<<endl;
+cin >> rok_urodzenia;
+cout <<"Twoj rok urodzenia to " << rok_urodzenia << endl;
+    return 0;
+}
